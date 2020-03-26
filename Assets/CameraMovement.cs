a34f756245945d50de755cbc0b38b2dc27164cb7
Madhav <madhavkhanna2021@u.northwestern.edu>
2020-03-26 11:19:54 -0500

    Initial commit

---
+++ Assets/CameraMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.W)) {
+            transform.position += transform.forward;
+        }
+        if (Input.GetKey(KeyCode.S)) {
+            transform.position -= transform.forward;
+        }
+
+        if (Input.GetKey(KeyCode.A)) {
+            transform.position -= transform.right;
+        }
+        if (Input.GetKey(KeyCode.D)) {
+            transform.position += transform.right;
+        }
+
+        transform.eulerAngles += new Vector3(Input.GetAxisRaw("Mouse Y"), Input.GetAxisRaw("Mouse X"), 0);
+
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# RayMarched_RayTracing
+ A Raytracer implementation in Unity Engine with the RayMarching Rendering Method Implemented in the Compute Shader
